f606b7fda10bb88fb1952b3211cc2c72c2e2cb72
Andrew <andrew.am2017@gmail.com>
2022-05-21 19:10:20 +0300

    перевел json в словарь, подготовил данные для страницы, вывел данные на странице

--- WeatherApp/weather/views.py
+++ WeatherApp/weather/views.py
@@ -8,11 +8,26 @@ from django.shortcuts import render
 def index(request):
     appid = '6a4f0328eeef57b74e12b8438db06807'
     city = 'Moscow'
-    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={appid}'
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={appid}'
 
-    res = requests.get(url)
+    res = requests.get(url).json()
+
+    city_info = {
+        'city': city,
+        'temp': res['main']['temp'],
+        'icon': res['weather'][0]['icon']
+    }
+
+    context = {
+        'info': city_info
+    }
+
+    print("Данные:")
+
+    for key in res.items():
+        print(key)
 
 
 
 
-    return render(request, 'weather/index.html')
+    return render(request, 'weather/index.html', context)
