c8db054ae1961b9b51351f35ac3aeab2df40378d
Andrew <andrew.am2017@gmail.com>
2022-05-22 00:07:05 +0300

    добавил сортировку информации по температуре

--- WeatherApp/weather/views.py
+++ WeatherApp/weather/views.py
@@ -5,11 +5,26 @@ from .forms import CityForm
 
 # Create your views here.
 
+def sort_info_by_temp(all_cities):
+    n = 0
+    index_list = []
+    for i in all_cities:
+        temp = i['temp']
+        index_list.append((temp, n))
+        n += 1
 
+    index_list.sort()
+
+    all_sorted_cities = []
+
+    for i in index_list:
+        ind = i[1]
+        all_sorted_cities.append(all_cities[ind])
+
+    return all_sorted_cities
 
 def index(request):
     appid = '6a4f0328eeef57b74e12b8438db06807'
-
 
     if (request.method == 'POST'):
         form = CityForm(request.POST)
@@ -19,7 +34,7 @@ def index(request):
 
     cities = City.objects.all()
 
-    all_cityes = []
+    all_cities = []
     for city in cities:
         url = f'https://api.openweathermap.org/data/2.5/weather?q={city.name}&units=metric&appid={appid}'
         res = requests.get(url).json()
@@ -30,21 +45,13 @@ def index(request):
             'icon': res['weather'][0]['icon']
         }
 
-        all_cityes.append(city_info)
+        all_cities.append(city_info)
 
-
-
-
+    all_sorted_cities = sort_info_by_temp(all_cities)
 
     context = {
-        'all_info': all_cityes,
+        'all_info': all_sorted_cities,
         'form': form
     }
-
-    #print(type(all_cityes[0]['temp']))
-    print(all_cityes)
-
-
-
 
     return render(request, 'weather/index.html', context)
