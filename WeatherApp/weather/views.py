2ca600ca15dac39d05293b4e314a98e1c8e367a1
Andrew <andrew.am2017@gmail.com>
2022-05-21 18:50:26 +0300

    подключил API openweathermap

--- WeatherApp/weather/views.py
+++ WeatherApp/weather/views.py
@@ -1,3 +1,4 @@
+import requests
 from django.shortcuts import render
 
 # Create your views here.
@@ -5,4 +6,13 @@ from django.shortcuts import render
 
 
 def index(request):
+    appid = '6a4f0328eeef57b74e12b8438db06807'
+    city = 'Moscow'
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={appid}'
+
+    res = requests.get(url)
+
+
+
+
     return render(request, 'weather/index.html')
